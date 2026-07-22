72ea89e81a9491ac7e77eba427a11752e1494845
daniel5zhu <daniel5zhu@gmail.com>
2026-07-22 10:44:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,23 @@ team_games = df[
     (df["homeTeam"] == team) | (df["awayTeam"] == team)
 ].copy()
 
+team_games["team_score"] = np.where(
+    team_games["homeTeam"] == selected_team,
+    team_games["homePoints"],
+    team_games["awayPoints"],
+)
+team_games["opp_score"] = np.where(
+    team_games["homeTeam"] == selected_team,
+    team_games["awayPoints"],
+    team_games["homePoints"],
+)
+team_games["opponent"] = np.where(
+    team_games["homeTeam"] == selected_team,
+    team_games["awayTeam"],
+    team_games["homeTeam"],
+)
+team_games["margin"] = team_games["team_score"] - team_games["opp_score"]
+
 team_games["location"] = np.where(
     team_games["homeTeam"] == team, "Home", "Away"
 )
